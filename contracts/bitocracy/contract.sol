42d822f824f05259b352cfe44118f60ffdd88a07
Tony Seing <tony.seing@gmail.com>
2016-04-30 20:30:52 -0400

    progress with scaffolding

--- contracts/bitocracy/contract.sol
+++ contracts/bitocracy/contract.sol
@@ -2,12 +2,47 @@
 contract Election
 {
 	uint choice;
+	mapping (address => uint) candidateTallies;
+	mapping (uint => address) candidateIndex;
+	uint candidateCount;
+	
+	struct candidate {
+		uint id;
+		string name;
+		string description;
+		address[] ballots;
+		uint tally;
+	}
+	
+	event Vote(uint choice_id);
+	
 	function Election(uint v) {
 		choice = v;
 	}
-	function vote(uint v) {
-		choice = v;
+	
+	function registerCandidate() {
+	 // @todo
 	}
+	
+	function getVoteTallies()
+	{
+		for(uint i = 0; i < candidateCount; i++)
+		{
+			//doSomeStuff(accountBalances[accountIndex[i]]);
+		}
+	}
+	
+	// checks if election is active or not
+	function isActive() returns (bool) {
+		return true;
+	}	
+	
+	function vote(uint v) {
+		if (isActive()) {
+			choice = v;
+		}
+	}
+	
 	function getVote() constant returns (uint) {
 		return choice;
 	}
